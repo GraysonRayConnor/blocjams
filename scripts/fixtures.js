4213ca358b77370d52612ef9589a66213d91c248
Grayson Connor <grayson.bolon@yahoo.com>
2021-01-26 17:34:40 -0600

    moved blocjams to new repo

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+       { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+          { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+          { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+          { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+          { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+      ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumWhite = {
+     title: 'The White Album',
+     artist: 'The Beatles',
+     label: 'Apple Records',
+     year: '1968',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Back In The U.S.S.R.', duration: '2:43' },
+         { title: 'Dear Prudence', duration: '3:56' },
+         { title: 'Glass Onion', duration: '2:17'},
+         { title: 'Ob-La-Di, Ob-La-Da', duration: '3:08' },
+         { title: 'Wild Honey Pie', duration: '0:58'}
+     ]
+ };
